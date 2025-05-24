05de3ed8dfc3a8b62758857bb5622a916fea923c
Samir Moreira Antonio <samir.ma@gmail.com>
2025-05-24 19:46:13 +0100

    reset

--- src/reconstruction/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file makes Python treat the `reconstruction` directory as a package.
-
-# Expose the PointCloudMapper class for easier importing
-from .pointcloud_mapper import PointCloudMapper
-
-__all__ = ['PointCloudMapper']
